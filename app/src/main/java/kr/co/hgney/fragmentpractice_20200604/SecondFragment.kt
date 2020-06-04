dbb3f9a0caa96ca3afd6e4d128cafa1d9a8ab606
khkssong6949 <bada6949@gmail.com>
2020-06-04 21:51:21 +0900

    프래그먼트 추가

--- app/src/main/java/kr/co/hgney/fragmentpractice_20200604/SecondFragment.kt
+++ app/src/main/java/kr/co/hgney/fragmentpractice_20200604/SecondFragment.kt
@@ -5,16 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.fragment_first.*
+import kotlinx.android.synthetic.main.fragment_second.*
 
-class FirstFragment : Fragment() {
+class SecondFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_second, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
